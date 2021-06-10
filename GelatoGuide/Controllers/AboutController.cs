e2da4cade954e5f64ca746c949705ee66e8b8b4c
Ivaylo Kostov <ikostov87@gmail.com>
2021-06-10 23:22:38 +0300

    all files have been restored

---
+++ GelatoGuide/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using GelatoGuide.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GelatoGuide.Controllers
+{
+    public class AboutController : Controller
+    {
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+    }
+}
