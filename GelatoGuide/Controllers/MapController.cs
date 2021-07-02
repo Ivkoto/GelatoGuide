c98d2d54b22e9f43cb249e921163d87c76a546ca
Ivaylo Kostov <ikostov87@gmail.com>
2021-07-03 02:45:47 +0300

    statement "c6e32c9" restored

--- GelatoGuide/Controllers/MapController.cs
+++ GelatoGuide/Controllers/MapController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace GelatoGuide.Controllers
 {
     public class MapController : Controller
-    { 
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MapController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
         public IActionResult Map() => View();
     }
 }
--- GelatoGuide/Program.cs
+++ GelatoGuide/Program.cs
@@ -1,6 +1,14 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 
 namespace GelatoGuide
 {
