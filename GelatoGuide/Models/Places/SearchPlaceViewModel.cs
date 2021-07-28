53ee08421d3b37aeffc59d4a1c8db6c3703c27c4
Ivaylo Kostov <ikostov87@gmail.com>
2021-07-28 23:33:02 +0300

    All blog articles view have been changed. Map view page have been updated. main layout have been updated.

---
+++ GelatoGuide/Models/Places/SearchPlaceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GelatoGuide.Models.Places
+{
+    public class SearchPlaceViewModel
+    {
+        public IEnumerable<string> SearchTerm { get; init; }
+        public IEnumerable<AllPlacesViewModel> Places { get; set; }
+    }
+}
