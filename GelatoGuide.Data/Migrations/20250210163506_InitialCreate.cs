91096cff22ccc8f38a07846cdd335436523bbe03
Ivaylo Kostov <Ivaylo.Kostov@devocean.solutions>
2025-02-10 19:03:40 +0200

    Migration to .Net 9. Moving to minimal hosting model.

---
+++ GelatoGuide.Data/Migrations/20250210163506_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GelatoGuide.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ShopItems",
+                type: "decimal(12,2)",
+                precision: 12,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(12,2)",
+                precision: 12,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ShopItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,2)",
+                oldPrecision: 12,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,2)",
+                oldPrecision: 12,
+                oldScale: 2);
+        }
+    }
+}
